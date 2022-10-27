266accfec6f5954567502ccd70548f33e77cadbb
polidex <lp.igor@gmail.com>
2022-10-27 12:57:41 -0300

    feat: reqt 06 done

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import amountValidation from '../middleware/amountValidation';
+import nameValidation from '../middleware/nameValidations';
 
 const productsRouter = Router();
 
 const productsController = new ProductsController();
 
-productsRouter.post('/', productsController.create);
+productsRouter.post('/', nameValidation, amountValidation, productsController.create);
 
 productsRouter.get('/', productsController.read);
 
